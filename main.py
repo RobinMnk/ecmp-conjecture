4d552db7382ddfe7e64b1c8ee891c3c1ae43d666
Robin Münk <RobinMnk@users.noreply.github.com>
2022-09-15 09:23:45 +0200

    added optimal solver

--- main.py
+++ main.py
@@ -25,36 +25,17 @@ def instance_to_dot(instance: Instance):
     dot = graphviz.Digraph('ecmp-test', comment='ECMP Test')
     dot.node(str(0), "target", color="red")
     for node in range(1, instance.dag.num_nodes):
-        dot.node(str(node), str(node + 1), color="blue" if node in instance.sources else "black")
+        dot.node(str(node), str(node), color="blue" if node in instance.sources else "black")
 
         for nb in instance.dag.neighbors[node]:
             dot.edge(str(node), str(nb))
 
-    print(dot.source)
-
-
-def _rec_generate_all_paths(G: DAG, node: int, target: int, visited: list, path: list):
-
-    if node == target:
-        yield " - ".join(map(lambda x: str(x+1), path))
-    else:
-        for nb in G.neighbors[node]:
-            if not nb in path:
-                path.append(nb)
-                yield from _rec_generate_all_paths(G, nb, target, visited, path)
-                path.pop()
-
-
-def generate_all_paths(G: DAG, source: int, target: int):
-    visited = [False for _ in range(G.num_nodes)]
-    yield from _rec_generate_all_paths(G, source, target, visited, [source])
+    s = graphviz.Source(dot.source, filename="graph/output", format="svg")
+    s.render(engine="circo")
 
 
 if __name__ == '__main__':
     random.seed(31415926535)
 
-    inst = build_random_DAG(5, 0.4)
+    inst = build_random_DAG(10, 0.4)
     instance_to_dot(inst)
-
-    for p in generate_all_paths(inst.dag, 2, 0):
-        print(p)
--- optimal_solver.py
+++ optimal_solver.py
@@ -4,18 +4,80 @@ import gurobipy as gp
 from gurobipy import GRB
 
 
+def _rec_generate_all_paths(G: DAG, node: int, target: int, visited: list, edge_dict: dict, path: list):
+    if node == target:
+        pathname = f"path:{'-'.join(map(lambda x: str(x), path))}"
+        for i in range(len(path) - 1):
+            edge = tuple([path[i], path[i+1]])
+            edge_dict[edge].append(pathname)
+        yield pathname
+    else:
+        for nb in G.neighbors[node]:
+            if nb not in path:
+                path.append(nb)
+                yield from _rec_generate_all_paths(G, nb, target, visited, edge_dict, path)
+                path.pop()
+
+
+def generate_all_paths(G: DAG, source: int, target: int, edge_dict: dict):
+    visited = [False for _ in range(G.num_nodes)]
+    yield from _rec_generate_all_paths(G, source, target, visited, edge_dict, [source])
+
+
 def calculate_optimal_solution(instance: Instance):
+    dag: DAG = instance.dag
+    sources = instance.sources
+    target = instance.target
+
     try:
         # Create a new model
         m = gp.Model("ecmp_opt")
 
+        """ Add Variables """
+        # Congestion variable
+        cong = m.addVar(name="cong", obj=1.0, lb=0, ub=GRB.INFINITY, vtype=GRB.CONTINUOUS, column=None)
 
+        # Add a variable for each source -> target path, for each source
+        path_vars = dict()
+        edge_dict = defaultdict(list)
+        for s in sources:
+            path_vars[s] = list()
+            for p in generate_all_paths(dag, s, target, edge_dict):
+                path_vars[s].append(
+                    m.addVar(name=p, obj=0.0, lb=0, ub=GRB.INFINITY, vtype=GRB.CONTINUOUS, column=None)
+                )
 
+        """ Set Objective """
+        m.setObjective(cong, GRB.MINIMIZE)
 
+        """ Add constraints """
+        for s in sources:
+            m.addConstr(sum(path_vars[s]) >= 1, name=f"source:{s}")
 
+        m.update()  # necessary to ensure we can access the variables by name below!
+        for k, v in edge_dict.items():
+            m.addConstr(sum([m.getVarByName(f"{name}") for name in v]) <= cong, name=f"edge:{k}")
+
+        """ Optimize """
+        m.optimize()
+
+        """ Output solution """
+        for v in m.getVars():
+            print('%s %g' % (v.VarName, v.X))
+
+        print('Obj: %g' % m.ObjVal)
 
     except gp.GurobiError as e:
         print('Error code ' + str(e.errno) + ': ' + str(e))
 
     except AttributeError:
         print('Encountered an attribute error')
+
+
+if __name__ == '__main__':
+    random.seed(31415926535)
+
+    inst = build_random_DAG(10, 0.4)
+    instance_to_dot(inst)
+
+    calculate_optimal_solution(inst)
